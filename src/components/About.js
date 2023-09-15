06f06a98a5bb9c1727392b1e8a1176c6c4379685
Bharatesh Poojary <bharateshpoojari@gmail.com>
2023-09-15 20:12:08 +0530

    fixed some issues

--- src/components/About.js
+++ src/components/About.js
@@ -3,6 +3,7 @@ import React from 'react'
 const About = () => {
   return (
     <div className='container ml-5 mt-5'>
+      
         <h1 className=' text-2xl'>First Reactjs based site <strong>Ganpati Bappa Moorya!!</strong></h1>
       <h1 className='text-xl'>This is a simple text utils website where you can manipulate your text according to your need. </h1>
     </div>
